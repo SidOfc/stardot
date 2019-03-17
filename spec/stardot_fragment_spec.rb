8fe39a4fac2c9a582cd1cf140cf96b1860a93546
Sidney Liebrand <sidneyliebrand@gmail.com>
2019-03-18 00:04:41 +0100

    Clean up fragment spec

--- spec/stardot_fragment_spec.rb
+++ spec/stardot_fragment_spec.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe Stardot::Fragment do
-  describe "#prompt" do
+  def reply_prompt_with(input, question, options, **settings)
+    answer = nil
+
+    with_cli_args '-i' do
+      frag = fragment
+
+      allow(frag).to receive(:read_input_char).and_return(input)
+      answer = frag.prompt(question, options, **settings)
+    end
+
+    answer
+  end
+
+  describe '#prompt' do
     it 'prompts for user input' do
-      answer = nil
-
-      with_cli_args '-i' do
-        frag = fragment
-
-        allow(frag).to receive(:read_input_char).and_return('y')
-        answer = frag.prompt('Can I ask a question?', %w[y n])
-      end
+      answer = reply_prompt_with 'y', 'prompt text', %w[y n]
 
       expect(answer).to be 'y'
     end
 
     it 'uses selected option when enter is pressed' do
-      answer = nil
-
-      with_cli_args '-i' do
-        frag = fragment
-
-        allow(frag).to receive(:read_input_char).and_return('')
-        answer = frag.prompt 'Can I ask a question?', %w[y n], selected: 'y'
-      end
+      answer = reply_prompt_with '', 'prompt text', %w[y n], selected: 'y'
 
       expect(answer).to be 'y'
     end
