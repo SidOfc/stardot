931b5dd68dbc0663f1ff04e07501fba69701c02e
Sidney Liebrand <sidneyliebrand@gmail.com>
2019-03-17 21:20:30 +0100

    Add a test, remove some files

--- spec/stardot_asdf_spec.rb
+++ spec/stardot_asdf_spec.rb
@@ -3,9 +3,9 @@
 RSpec.describe 'Asdf < Stardot::Fragment' do
   describe '#install' do
     it 'fails when language plugin is not installed' do
-      as_plugin :asdf do
-        install :does_not_exist, versions: :latest
-      end
+      asdf = as_plugin :asdf
+
+      asdf.process { install :does_not_exist, versions: :latest }
 
       expect(Stardot.logger.entries.last[:status]).to eq :error
     end
@@ -19,6 +19,19 @@ RSpec.describe 'Asdf < Stardot::Fragment' do
       asdf.process { install :ruby, versions: '2.5.0' }
 
       expect(Stardot.logger.entries.last[:status]).to eq :ok
+    end
+
+    it 'skips when specified language version is already installed' do
+      asdf = as_plugin :asdf
+
+      allow(asdf).to receive(:persist).and_return(true)
+      allow(asdf).to receive(:plugin?).with(:ruby).and_return(true)
+      allow(asdf).to receive(:language_installed?)
+        .with(:ruby, '3.0.0').and_return(true)
+
+      asdf.process { install :ruby, versions: '3.0.0' }
+
+      expect(Stardot.logger.entries.last[:status]).to eq :info
     end
   end
 end
